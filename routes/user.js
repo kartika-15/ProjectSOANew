f18888bfce9bd06ab90c47c2e147a4649712e986
mico218116764 <72438982+mico218116764@users.noreply.github.com>
2021-06-25 00:41:29 +0700

    Ming
    Done

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const exp= require('express');
 const db = require('../database');
+const jwt = require("jsonwebtoken");
 
 const route = exp.Router();
 
@@ -43,6 +44,13 @@ route.post('/register', async function (req, res) {
             error : "Email sudah terdaftar!"
         })
     }else {
+        if(email == null || pass == null || nama == null){
+            conn.release();
+            return res.status(404).json({
+                status : 404,
+                error : "Masukan inputan dengan benar"
+            })
+        }
         let q = await db.executeQuery(conn, `
             INSERT INTO user VALUES ('${email}', '${pass}', '${nama}','N', '${api}')
         `);
@@ -52,7 +60,6 @@ route.post('/register', async function (req, res) {
             nama : nama,
             api_key : api
         };
-        conn.release();
 
         return res.status(201).json({
             status : 201,
@@ -62,84 +69,44 @@ route.post('/register', async function (req, res) {
     }
 });
 
-route.put('/profile', async function (req, res) {
-    if(!req.headers['x-auth-token']){
-        return res.status(403).json({
-            status: 403,
-            error: "No Token!"
+route.get('/login', async function (req, res) {
+    let email = req.body.email;
+    let pass = req.body.password;
+    let epas = 'admin';
+    let api = genAPIKey(10);
+    
+    let conn = await db.getConn();
+    let check = await db.executeQuery(conn, `
+        SELECT * FROM user WHERE email='${email}' and password = '${pass}'
+    `);
+
+    if(check.length > 0){
+        conn.release();
+        return res.status(200).json({
+            status : 200,
+            message : "Api Key anda = "+ check[0].api_key
+        })
+    }else if(email == epas && pass == epas){
+        const date = new Date();
+        let tgl = date.toISOString().substr(0, 4) + "/" + date.toISOString().substr(5, 2) + "/" + date.toDateString().substr(8, 2) + date.toString().substr(15, 9);
+        
+        let token = jwt.sign(
+            {
+                "login_time" : tgl,
+            },
+            "minadmin",{expiresIn:'1H'}
+        );
+
+        return res.status(200).send({"token":token});
+    }
+    else {
+        return res.status(404).json({
+            status : 404,
+            message : "Email / Password salah"
         });
-    }else {
-        if(!req.body.new_pass){
-            let conn = db.getConn();
-            let q = db.executeQuery(conn, `
-                SELECT * FROM user WHERE api_key='${req.headers['x-auth-token']}'
-            `);
-
-            if(q.length > 0){
-                let q2 = db.executeQuery(conn, `
-                    UPDATE user SET nama='${req.body.nama}'
-                `);
-
-                const hasil = {
-                    email : q[0].email,
-                    nama : req.body.nama
-                };
-                conn.release();
-                
-                return res.status(200).json({
-                    status : 200,
-                    message : "Berhasil Ganti Nama",
-                    result : hasil
-                });
-            }else {
-                return res.status(403).json({
-                    status: 403,
-                    error: "Token Not Valid!"
-                });
-            }
-        }else if(req.body.new_pass && !req.body.old_pass){
-            return res.status(403).json({
-                status: 403,
-                error: "Password lama tidak ada!"
-            });
-        }else if(!req.body.new_pass && req.body.old_pass){
-            return res.status(403).json({
-                status: 403,
-                error: "Password baru tidak ada!"
-            });
-        }else if(!req.body.nama){
-            let conn = db.getConn();
-            let q = db.executeQuery(conn, `
-                SELECT * FROM user WHERE api_key='${req.headers['x-auth-token']}'
-            `);  
-
-            if(q.length > 0){
-                if(req.body.old_pass == q[0].password){
-                    let q2 = db.executeQuery(conn, `
-                        UPDATE user SET pass='${req.body.new_pass}'
-                    `);
-
-                    conn.release();
-                    
-                    return res.status(200).json({
-                        status : 200,
-                        message : "Berhasil Ganti Password"
-                    });
-                }else {
-                    return res.status(403).json({
-                        status: 403,
-                        error: "Password Lama Salah!"
-                    });
-                }
-            }else {
-                return res.status(403).json({
-                    status: 403,
-                    error: "Token Not Valid!"
-                });
-            }
-        }
     }
 });
+
 
 
 module.exports = route;
